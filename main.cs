17f9c6b6ff3a70ff131fa5c25c0fefa1c2e2d8f2
renzmendoza810 <31803655-renzmendoza810@users.noreply.replit.com>
2024-02-03 02:50:10 +0000

    Initial commit

---
+++ main.cs
@@ -0,0 +1,11 @@
+using System;
+
+class Program {
+  public static void Main (string[] args) {
+    string firstname = "Nico";
+    string lastname = "abella";
+    int age = 18 ;
+    Console.WriteLine ($"Hello World {firstname} {lastname} Age: {age}");
+    Console.WriteLine ($"Hello World " + firstname + " " + lastname + " Age: " + age);
+  }
+}
